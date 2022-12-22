f033d3e5f3ad3c03a6580d91cd75e35b07727ebf
Ayush Rout <axr6077@rit.edu>
2022-12-21 21:14:54 -0800

    func decl for printing usage info and cmd line arg parsing

--- include/cmd_args.h
+++ include/cmd_args.h
@@ -2,7 +2,13 @@
 #define CMD_ARGS_H
 
 typedef struct CmdArgs {
-
+	int num_dendrs;
+	int num_comps;
 } CmdArgs;
+
+// print usage info
+void usage (char *name);
+// parse command line arguments
+int parseArgs(CmdArgs *cmd_args, int argc, char **argv);
 
 #endif
