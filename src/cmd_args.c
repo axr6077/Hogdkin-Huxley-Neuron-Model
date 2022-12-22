4bc069f59076e8491b214b09d3235e4aed6ee64d
Ayush Rout <axr6077@rit.edu>
2022-12-21 21:17:30 -0800

    command line args impl

---
+++ src/cmd_args.c
@@ -0,0 +1,13 @@
+#include "cmd_args.h"
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+void usage(char *name) {
+
+}
+
+int parseArgs(CmdArgs *cmd_args, int argc, char **argv) {
+
+}
